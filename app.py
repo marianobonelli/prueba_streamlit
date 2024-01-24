0676ab41d3a36940ce89bf57d40b878001ce3c9c
marianobonelli <mbonelli95@gmail.com>
2024-01-24 20:40:32 -0300

    txt

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tablero de Lotes y Cultivos",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Aplicacion de prueba 123")
+
+with st.sidebar:
+    st.title("sidebar")
+
